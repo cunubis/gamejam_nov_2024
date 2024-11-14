dc6394fc82451e90d5c6f6a398d5d905faa776bd
Malte <cunubis@gmail.com>
2024-11-14 22:00:10 +0100

    Added HUD and Global var back

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -6,12 +6,17 @@ var elapsed_time: int = 0
 @onready var game_timer = $TimerLabel/Timer
 @onready var time_label = $TimerLabel 
 @onready var score_label = $ScoreLabel
+@onready var pause_label = $PauseLabel
+@onready var resume_button = $resumeButton
 
 func _ready():
 	# Start the timer with an interval of 1 second
 	game_timer.start(1)
 	update_timer_label()  # Initialize the label with the starting time
 	update_score_label() #  Initialize the label with the coin score
+	pause_label.hide() # hide pause on start
+	resume_button.hide() # hide resume button on start
+	resume_button.process_mode = Node.PROCESS_MODE_WHEN_PAUSED  # Enable processing when paused
 
 func _process(delta):
 	# Check and update the label text to display the global variable
@@ -34,3 +39,13 @@ func update_timer_label():
 #SCORE
 func update_score_label():
 	score_label.text = "Score: " +str(Global.coinScore)
+
+func _on_pause_button_pressed():
+	get_tree().paused = true
+	resume_button.show()
+	pause_label.show()
+	
+func _on_resume_button_pressed():
+	get_tree().paused = false
+	resume_button.hide()
+	pause_label.hide()
